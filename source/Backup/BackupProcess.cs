60990427480b7199eaa8008e637196fc8296420e
moacirhoeckele <moacirhoeckele@gmail.com>
2016-04-28 13:57:04 -0300

    Some improvements has been done.

---
+++ source/Backup/BackupProcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Backup
+{
+    public class BackupProcess
+    {
+        public string Destination { get; set; }
+
+        public string[] Sources { get; set; }
+
+        public IEnumerable<string> Options { get; set; }
+
+        public DateTime StartedTime { get; set; }
+
+        public List<FileInfo> SourceFiles { get; set; }
+
+        public List<FileInfo> CopiedFiles { get; set; }
+
+        public List<FileInfo> DeletedFiles { get; set; }
+
+        public string BackupBaseFolder { get; set; }
+
+        public string BackupBasePath { get; set; }
+
+        public List<FileInfo> DestinationFiles { get; set; }
+    }
+}
